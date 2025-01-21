2c57b6f35a77d89a3f590ba4fb4ef92dce1d6e2e
aby913 <aby913@163.com>
2025-01-21 23:31:24 +0800

    ci: build wsl-msi script fix (#907)
    ci: build script fix

--- scripts/build-wsl-install-msi.sh
+++ scripts/build-wsl-install-msi.sh
@@ -4,11 +4,11 @@ set -o pipefail
 set -xe
 
 curl -Lo wsl.2.3.26.0.amd64.msi https://github.com/microsoft/WSL/releases/download/2.3.26/wsl.2.3.26.0.x64.msi
-wsl_2-3-26=$(md5sum wsl.2.3.26.0.amd64.msi|awk '{print $1}')
+wsl_2_3_26=$(md5sum wsl.2.3.26.0.amd64.msi|awk '{print $1}')
 
-aws s3 cp wsl.2.3.26.0.amd64.msi s3://terminus-os-install/${wsl_2-3-26} --acl=public-read
+aws s3 cp wsl.2.3.26.0.amd64.msi s3://terminus-os-install/${wsl_2_3_26} --acl=public-read
 
 curl -Lo wsl.2.3.26.0.arm64.msi https://github.com/microsoft/WSL/releases/download/2.3.26/wsl.2.3.26.0.arm64.msi
-wsl_2-3-26_arm64=$(md5sum wsl.2.3.26.0.arm64.msi|awk '{print $1}')
+wsl_2_3_26_arm64=$(md5sum wsl.2.3.26.0.arm64.msi|awk '{print $1}')
 
-aws s3 cp wsl.2.3.26.0.arm64.msi s3://terminus-os-install/arm64/${wsl_2-3-26_arm64} --acl=public-read
+aws s3 cp wsl.2.3.26.0.arm64.msi s3://terminus-os-install/arm64/${wsl_2_3_26_arm64} --acl=public-read
