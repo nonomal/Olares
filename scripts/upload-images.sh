e5762cc3855bbcb9a102771e23e281b9c84f3039
liuyu <>
2025-02-10 13:22:18 +0800

    olares: upload original file with md5 as a backup

--- scripts/upload-images.sh
+++ scripts/upload-images.sh
@@ -13,18 +13,28 @@ cat $1|while read image; do
 
     curl -fsSLI https://dc3p1870nn3cj.cloudfront.net/$path$name.tar.gz > /dev/null
     if [ $? -ne 0 ]; then
-        set -e
-        docker pull $image
-        docker save $image -o $name.tar
-        gzip $name.tar
+        code=$(curl -o /dev/null -fsSLI -w "%{http_code}" https://dc3p1870nn3cj.cloudfront.net/$path$name.tar.gz)
+        if [ $code -eq 403 ]; then
+            set -e
+            docker pull $image
+            docker save $image -o $name.tar
+            gzip $name.tar
 
-        md5sum $name.tar.gz > $checksum
+            md5sum $name.tar.gz > $checksum
+            backup_file=$(cat $checksum)
 
-        aws s3 cp $name.tar.gz s3://terminus-os-install/$path$name.tar.gz --acl=public-read
-        aws s3 cp $checksum s3://terminus-os-install/$path$checksum --acl=public-read
-        echo "upload $name completed"
-        
-        set +e
+            aws s3 cp $name.tar.gz s3://terminus-os-install/$path$name.tar.gz --acl=public-read
+            aws s3 cp $name.tar.gz s3://terminus-os-install/backup/$path$backup_file --acl=public-read
+            aws s3 cp $checksum s3://terminus-os-install/$path$checksum --acl=public-read
+            echo "upload $name completed"
+            
+            set +e
+        else
+            if [ $code -ne 200  ]; then
+                echo  "failed to check image"
+                exit -1
+            fi
+        fi
     fi
 
     
@@ -32,17 +42,27 @@ cat $1|while read image; do
     # re-upload checksum.txt
     curl -fsSLI https://dc3p1870nn3cj.cloudfront.net/$path$checksum > /dev/null
     if [ $? -ne 0 ]; then
-        set -e
-        docker pull $image
-        docker save $image -o $name.tar
-        gzip $name.tar
+        code=$(curl -o /dev/null -fsSLI -w "%{http_code}" https://dc3p1870nn3cj.cloudfront.net/$path$checksum)
+        if [ $code -eq 403 ]; then
+            set -e
+            docker pull $image
+            docker save $image -o $name.tar
+            gzip $name.tar
 
-        md5sum $name.tar.gz > $checksum
+            md5sum $name.tar.gz > $checksum
+            backup_file=$(cat $checksum)
 
-        aws s3 cp $name.tar.gz s3://terminus-os-install/$path$name.tar.gz --acl=public-read
-        aws s3 cp $checksum s3://terminus-os-install/$path$checksum --acl=public-read
-        echo "upload $name completed"
-        set +e
+            aws s3 cp $name.tar.gz s3://terminus-os-install/$path$name.tar.gz --acl=public-read
+            aws s3 cp $name.tar.gz s3://terminus-os-install/backup/$path$backup_file --acl=public-read
+            aws s3 cp $checksum s3://terminus-os-install/$path$checksum --acl=public-read
+            echo "upload $name completed"
+            set +e
+        else
+            if [ $code -ne 200  ]; then
+                echo  "failed to check image"
+                exit -1
+            fi
+        fi
     fi
 
     # upload to tencent cloud cos
