c290145ea84be9c9e4c51842813561db694c7209
hysyeah <hysyeah@gmail.com>
2025-02-12 22:51:28 +0800

    app-service: continue to resume op after restart; envoy inbound tcp proxy (#943)
    * app-service: continue to resume op after restart; envoy inbound tcp proxy
    
    * ci: fix upload script bug
    
    ---------
    
    Co-authored-by: liuyu <>

--- scripts/upload-images.sh
+++ scripts/upload-images.sh
@@ -15,24 +15,25 @@ cat $1|while read image; do
     if [ $? -ne 0 ]; then
         code=$(curl -o /dev/null -fsSLI -w "%{http_code}" https://dc3p1870nn3cj.cloudfront.net/$path$name.tar.gz)
         if [ $code -eq 403 ]; then
-            set -e
+            set -ex
             docker pull $image
             docker save $image -o $name.tar
             gzip $name.tar
 
             md5sum $name.tar.gz > $checksum
-            backup_file=$(cat $checksum)
+            backup_file=$(awk '{print $1}' $checksum)
             if [ x"$backup_file"  == x""  ]; then
                 echo  "invalid checksum"
                 exit 1
             fi
 
+            echo "start to upload [$name.tar.gz]"
             aws s3 cp $name.tar.gz s3://terminus-os-install/$path$name.tar.gz --acl=public-read
             aws s3 cp $name.tar.gz s3://terminus-os-install/backup/$path$backup_file --acl=public-read
             aws s3 cp $checksum s3://terminus-os-install/$path$checksum --acl=public-read
             echo "upload $name completed"
             
-            set +e
+            set +ex
         else
             if [ $code -ne 200  ]; then
                 echo  "failed to check image"
@@ -48,13 +49,13 @@ cat $1|while read image; do
     if [ $? -ne 0 ]; then
         code=$(curl -o /dev/null -fsSLI -w "%{http_code}" https://dc3p1870nn3cj.cloudfront.net/$path$checksum)
         if [ $code -eq 403 ]; then
-            set -e
+            set -ex
             docker pull $image
             docker save $image -o $name.tar
             gzip $name.tar
 
             md5sum $name.tar.gz > $checksum
-            backup_file=$(cat $checksum)
+            backup_file=$(awk '{print $1}' $checksum)
             if [ x"$backup_file"  == x""  ]; then
                 echo  "invalid checksum"
                 exit 1
@@ -64,7 +65,7 @@ cat $1|while read image; do
             aws s3 cp $name.tar.gz s3://terminus-os-install/backup/$path$backup_file --acl=public-read
             aws s3 cp $checksum s3://terminus-os-install/$path$checksum --acl=public-read
             echo "upload $name completed"
-            set +e
+            set +ex
         else
             if [ $code -ne 200  ]; then
                 echo  "failed to check image"
