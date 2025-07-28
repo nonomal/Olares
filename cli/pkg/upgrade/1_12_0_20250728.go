00bf6290649a1bfdcb2a6d55731bb2bb57878f33
dkeven <dkvvven@gmail.com>
2025-07-28 19:49:28 +0800

    feat: add task to upgrade k3s

---
+++ cli/pkg/upgrade/1_12_0_20250728.go
@@ -0,0 +1,100 @@
+package upgrade
+
+import (
+	"fmt"
+	"github.com/Masterminds/semver/v3"
+	"github.com/beclab/Olares/cli/pkg/bootstrap/precheck"
+	"github.com/beclab/Olares/cli/pkg/common"
+	"github.com/beclab/Olares/cli/pkg/core/connector"
+	"github.com/beclab/Olares/cli/pkg/core/task"
+	"github.com/beclab/Olares/cli/pkg/core/util"
+	k3stemplates "github.com/beclab/Olares/cli/pkg/k3s/templates"
+	"github.com/beclab/Olares/cli/pkg/manifest"
+	"github.com/beclab/Olares/cli/pkg/terminus"
+	"github.com/pkg/errors"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type upgrader_1_12_0_20250728 struct {
+	upgraderBase
+}
+
+func (u upgrader_1_12_0_20250728) Version() *semver.Version {
+	return semver.MustParse("1.12.0-20250728")
+}
+
+func (u upgrader_1_12_0_20250728) PrepareForUpgrade() []task.Interface {
+	var preTasks []task.Interface
+	if util.IsExist(filepath.Join("/etc/systemd/system/", k3stemplates.K3sService.Name())) {
+		preTasks = append(preTasks,
+			&task.LocalTask{
+				Name:   "UpgradeK3sBinary",
+				Action: new(upgradeK3sBinary),
+			},
+			&task.LocalTask{
+				Name:   "UpdateK3sServiceEnv",
+				Action: new(injectK3sCertExpireTime),
+			},
+			&task.LocalTask{
+				Name: "RestartK3sService",
+				Action: &terminus.SystemctlCommand{
+					UnitNames:           []string{common.K3s},
+					Command:             "restart",
+					DaemonReloadPreExec: true,
+				},
+			},
+			&task.LocalTask{
+				Name:   "WaitForKubeAPIServerUp",
+				Action: new(precheck.GetKubernetesNodesStatus),
+				Retry:  10,
+			})
+	}
+	return append(preTasks, u.upgraderBase.PrepareForUpgrade()...)
+}
+
+type upgradeK3sBinary struct {
+	common.KubeAction
+}
+
+func (u *upgradeK3sBinary) Execute(runtime connector.Runtime) error {
+	m, err := manifest.ReadAll(u.KubeConf.Arg.Manifest)
+	if err != nil {
+		return err
+	}
+	binary, err := m.Get(common.K3s)
+	if err != nil {
+		return fmt.Errorf("get k3s binary info failed: %v", err)
+	}
+
+	path := binary.FilePath(runtime.GetBaseDir())
+	dst := filepath.Join(common.BinDir, common.K3s)
+	// replacing the binary does not interrupt the running k3s server
+	if err := runtime.GetRunner().SudoScp(path, dst); err != nil {
+		return errors.Wrap(errors.WithStack(err), fmt.Sprintf("upgrade k3s binary failed"))
+	}
+	if _, err := runtime.GetRunner().SudoCmd(fmt.Sprintf("chmod +x %s", dst), false, false); err != nil {
+		return err
+	}
+	return nil
+}
+
+type injectK3sCertExpireTime struct {
+	common.KubeAction
+}
+
+func (u *injectK3sCertExpireTime) Execute(runtime connector.Runtime) error {
+	expireTimeEnv := "CATTLE_NEW_SIGNED_CERT_EXPIRATION_DAYS"
+	envFile := filepath.Join("/etc/systemd/system/", k3stemplates.K3sServiceEnv.Name())
+	content, err := os.ReadFile(envFile)
+	if err != nil && !os.IsNotExist(err) {
+		return fmt.Errorf("failed to read k3s service env file: %v", err)
+	}
+	if strings.Contains(string(content), expireTimeEnv) {
+		return nil
+	}
+	newContent := string(content) + fmt.Sprintf("\n%s=36500", expireTimeEnv)
+	err = os.WriteFile(envFile, []byte(newContent), 0644)
+	return err
+}
