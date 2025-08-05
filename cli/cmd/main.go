916e9f8a0376fa76d6c87a4ebf9856484e5f1a15
dkeven <dkvvven@gmail.com>
2025-08-05 14:42:21 +0800

    chore(cli): remove any call to ulimit that has no effect

--- cli/cmd/main.go
+++ cli/cmd/main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"os"
-	"os/exec"
-
 	"github.com/beclab/Olares/cli/cmd/ctl"
+	"os"
 )
 
 func main() {
 	cmd := ctl.NewDefaultCommand()
-	_ = exec.Command("/bin/bash", "-c", "ulimit -u 65535").Run()
-	_ = exec.Command("/bin/bash", "-c", "ulimit -n 65535").Run()
 
 	if err := cmd.Execute(); err != nil {
 		// fmt.Println(err)
